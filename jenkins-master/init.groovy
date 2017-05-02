1a12b3d72facb903589dd39a84f1bddf4656285c
ajyura <adam.j.yura@accenture.com>
2017-05-02 11:17:10 -0400

    Update init.groovy

--- jenkins-master/init.groovy
+++ jenkins-master/init.groovy
@@ -10,3 +10,8 @@ Thread.start {
       Jenkins.instance.setSlaveAgentPort(port)
       println "--> setting agent port for jnlp... done"
 }
+
+Thread.start {
+	System.setProperty("hudson.model.DirectoryBrowserSupport.CSP",
+ 	"sandbox allow-scripts allow-same-origin; default-src 'none'; img-src 'self'; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline';")
+}
