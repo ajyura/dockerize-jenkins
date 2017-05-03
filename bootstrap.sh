078dfd09c0ea139177059e14ff0756d45ddd46d5
ajyura <adam.j.yura@accenture.com>
2017-05-03 17:44:39 -0400

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 
 # generate ssh key pair and move to authorized_keys
 cd ./jenkins-slave/files
-ssh-keygen -t rsa -b 4096 yes "" -P "passphrase" -f "./id_rsa" -q
+ssh-keygen -t rsa -b 4096 -P "passphrase" -f "./id_rsa" -q
 mv -f id_rsa.pub authorized_keys
 
 make build
